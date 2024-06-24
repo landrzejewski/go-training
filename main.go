f9bb80c794a1196ab9ea9e6feb6c656dea9cb2d2
landrzejewski <landrzejewski.poczta@gmail.com>
2024-06-24 10:53:09 +0200

    Basics

--- main.go
+++ main.go
@@ -75,5 +75,127 @@ func main() {
 	fmt.Printf("Procent (escape) %%\n")
 	fmt.Printf("Wartość z paddingiem %010.2f", 1100.232232); // padding symbol_wilekość paddingu_precyzja_formatowanie 
 
+	/*
+	Operatory arytmetyczne
+	+	dodawanie	
+    -	odejmowanie	
+    *	mnożenie
+    /	dzielenie
+    %	dzielenie modulo
+    ++	inkrementacja
+    --	dekrementacja
+	*/
 
+	var value = 100
+	var otherValue = 20.0
+	var calculationResult = float64(value) * otherValue // typ musi być jawanie skonwertowany
+	fmt.Printf("Result: %.2f\n", calculationResult)
+
+	/*
+	Operatory przypisania
+	=    przypisanie
+	+=, -=, *=, /=, %=, &=, |=, ^=, >>=, <<=  skrócony zapis x = x [operator] x 
+	*/   
+
+	/*
+	Operatory porównania
+	==   równość
+	!=   nierówność
+	>    większy
+	<    mniejszy
+	>=   większy/równy
+	<=   mniejszy/równy
+	*/
+
+	/*
+	Operatory logiczne 
+	&&   i
+	||   lub
+	!    zaprzeczenie 
+	*/
+
+	/*
+	Operatory bitowe
+	&    i
+	|    lub
+	^    xor
+	<<   przesunięcie bitów w lewo 
+	>>   przesunięcie bitów w prawo 
+	*/
+
+	// Instrukcja warunkowa if
+
+	inputValue := 5
+
+	if value % 2 == 0 { // wynaga wyrażenia zwracającego bool, nie zapisujemy nawiasów
+		fmt.Printf("Value %v is even\n", inputValue)
+	} else {
+		fmt.Printf("Value %v is not even\n", inputValue)
+	}
+
+	// wyrażenia logiczne mogą być skracane, jeśli ich rezultat jest znany po rozwinięciu ich części
+	// blok else jest opcjonalny
+	// można dodać wiele bloków if else
+	
+	// Instrukcja switch
+
+	switch inputValue {
+	case 1:
+		fmt.Println("First")
+	case 2:
+		fmt.Println("Second")
+	case 3, 4, 5:
+		fmt.Println("Small values")	
+	default:
+		fmt.Println("Other")
+	}
+
+	/*
+	var otherOnputValue any;
+
+	switch otherOnputValue.(type) {
+	case bool:
+		fmt.Println("Bool")
+	case int:
+		fmt.Println("Int")
+	default: 	
+		fmt.Println("Unknown")
+	}
+	*/
+
+	// Pętle
+
+	for i := 0; i < 10; i++ {
+		if i == 5 {
+			continue
+		}
+		fmt.Printf("Counter: %v\n", i)
+		if i > 7 {
+			break
+		}
+	}
+
+	j := 0
+	for j <= 10 {
+		fmt.Printf("Counter: %v\n", j)
+		j++
+	}
+
+	for x := range 5 {
+		fmt.Printf("Counter: %v\n", x)
+	}
+
+	colors := [3]string { "red", "blue", "yellow" }
+	for idx, color := range colors {
+		fmt.Printf("Color: %v has idex %v\n", color, idx)
+	}
+
+	for {
+		fmt.Printf("Tick\n")
+		break
+	}
+}
+
+func main() {
+	
 }
