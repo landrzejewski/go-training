4dc8a2973f975b4ca51c933325d791d800f12629
landrzejewski <landrzejewski.poczta@gmail.com>
2024-06-26 14:57:44 +0200

    concurrency

--- concurrency/producer_consumer_channels.go
+++ concurrency/producer_consumer_channels.go
@@ -9,6 +9,7 @@ import (
 func producer(index int, ch chan string, wg *sync.WaitGroup) {
 	for i := 0; i < 5; i++ {
 		ch <- fmt.Sprintf("Producer %v send %v", index, i)
+		time.Sleep(1 * time.Second)
 	}
 	wg.Done()
 }
@@ -17,12 +18,12 @@ func consumer(index int, ch chan string, wg *sync.WaitGroup) {
 	done := false
 	for !done {
 		select {
-	 	case msg, ok := <-ch:
-	  		if !ok {
+	 	case msg, isCLosed := <-ch:
+	  		if !isCLosed {
 	   			done = true
 	  		}
 	  		fmt.Printf("Consumer %v Received: %s\n", index, msg)
-	 	case <-time.After(1 * time.Second):
+	 	case <-time.After(10 * time.Second):
 	  		wg.Done()
 	 	}
    
@@ -34,12 +35,12 @@ func ProducerConsumer() {
 	ch := make(chan string)
 	var wg sync.WaitGroup
    
-	for i := 0; i < 50; i++ {
+	for i := 0; i < 2; i++ {
 		wg.Add(1)
 	 	go producer(i, ch, &wg)
 	}
-	
-	for i := 0; i < 100; i++ {
+
+	for i := 0; i < 1; i++ {
 		wg.Add(1)
 	 	go consumer(i, ch, &wg)
 	}
