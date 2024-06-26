c276801dfe5c7dade1c21be89690616aab08a3c5
landrzejewski <landrzejewski.poczta@gmail.com>
2024-06-26 10:31:18 +0200

    concurrency

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -2,7 +2,6 @@ package concurrency
 
 import (
 	"fmt"
-	"sync"
 	"time"
 )
 
@@ -110,7 +109,7 @@ func (ss *safeSlice) get(index int) (int, bool) {
 	if index < 0 || index >= len(ss.data) {
 		return 0, false
 	}
-	return ss.data[index], true	
+	return ss.data[index], true
 }
 
 func (ss *safeSlice) size() int {
@@ -231,13 +230,83 @@ func work() {
 func Run() {
 	go work()
 	go spend()
-	
+
 	time.Sleep(10 * time.Second)
 	fmt.Println("Current value:", money)
 }
 */
 
-func Run() {
-	
+// Deadlocks
+
+/*
+var (
+	lock1 = sync.Mutex{}
+	lock2 = sync.Mutex{}
+)
+
+func blueRobot() {
+	for {
+		fmt.Println("Blue: Acquiring lock1")
+		lock1.Lock()
+		fmt.Println("Blue: Acquiring lock2")
+		lock2.Lock()
+		fmt.Println("Blue: Both locks Acquired")
+		lock1.Unlock()
+		lock2.Unlock()
+		fmt.Println("Blue: Locks Released")
+	}
 }
 
+func redRobot() {
+	for {
+		fmt.Println("Red: Acquiring lock2")
+		lock2.Lock()
+		fmt.Println("Red: Acquiring lock1")
+		lock1.Lock()
+		fmt.Println("Red: Both locks Acquired")
+		lock1.Unlock()
+		lock2.Unlock()
+		fmt.Println("Red: Locks Released")
+	}
+}
+
+func Run() {
+	go redRobot()
+	go blueRobot()
+	time.Sleep(20 * time.Second)
+	fmt.Println("Done")
+}
+*/
+
+// Atomics
+
+var (
+	money int64 = 100
+	value = 10
+)
+
+func spend() {
+	for i := 1; i < 500; i++ {
+		//atomic.AddInt64(&money, int64(-value))
+		money -= int64(value)
+		fmt.Println("Spend: ", money)
+	}
+	fmt.Println("Spend: Done")
+}
+
+func work() {
+	for i := 1; i < 500; i++ {
+		// atomic.AddInt64(&money, int64(value))
+		money += int64(value)
+		fmt.Println("New income, current value:", money)
+	}
+	fmt.Println("Work: Done")
+}
+
+func Run() {
+	go work()
+	go spend()
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("Current value:", money)
+}
