c7f7eaf19b6f9b902db9109618e226f12ffa103f
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-10-22 16:52:00 +0200

    Basics

--- concurrency/flat_file_database.go
+++ concurrency/flat_file_database.go
@@ -1,7 +1,7 @@
 /*
 Stwórsz bazę danych opartą o plik płaski przechowującą dane w postaci binarnej (https://gobyexample.com/reading-files).
 Baza powinna umożliwiać wykonywanie następujących operacje: ADD, READ, UPDATE, DELETE na podstwie podango id rekordu.
-W celu uzyskania lepszej wydajnoći, wprowadź indeksowanie pozycji rekordu w pliku oraz pamięć podręczną (wykorzystaj mapy).
+W celu uzyskania lepszej wydajnoci, wprowadź indeksowanie pozycji rekordu w pliku oraz pamięć podręczną (wykorzystaj mapy).
 Pomyśl o optymalnym sposobie usuwania rekordów i ponownym wykorzystaniem miejsca po usuniętym rekordzie.
 Załóż, że każdy rekord ma stałą długość (ilośc bajtów)
 */
@@ -29,25 +29,28 @@ type userRecord struct {
 }
 
 func (u *userRecord) getId() int64 {
-	return fromBytes(u.Id[:])
+	return fromBytes[int64](u.Id[:])
 }
 
 func (u *userRecord) getFirstName() string {
-	return string(u.FirstName[:])
+	return fromBytes[string](u.FirstName[:])
 }
 
 func (u *userRecord) getLastName() string {
-	return string(u.LastName[:])
+	return fromBytes[string](u.LastName[:])
 }
 
 func (u *userRecord) getAge() int64 {
-	return fromBytes(u.Age[:])
+	return fromBytes[int64](u.Age[:])
 }
 
 func newUserRecord(id int64, firstName, lastName string, age int64) *userRecord {
 	var firstNameBytes [firstNameSize]byte
 	copy(firstNameBytes[:], firstName)
 
+	if len(lastName) > lastNameSize {
+		panic("Last name too long")
+	}
 	var lastNameBytes [lastNameSize]byte
 	copy(lastNameBytes[:], lastName)
 
@@ -59,16 +62,27 @@ func newUserRecord(id int64, firstName, lastName string, age int64) *userRecord 
 	}
 }
 
-func int64ToBytes(value int64) (arr [8]byte) {
-	binary.LittleEndian.PutUint64(arr[0:8], uint64(value))
-	return
+type Serializable interface {
+	int32 | int64 | string
 }
 
-func fromBytes[T int64](data []byte) (value T) {
+func fromBytes[T Serializable](data []byte) (value T) {
 	err := binary.Read(bytes.NewReader(data), binary.LittleEndian, &value)
 	if err != nil {
 		panic("Reading bytes failed")
 	}
+	return
+}
+
+func stringToBytes[T Serializable](text string, maxSize int) []byte {
+	if len(text) > maxSize {
+		panic("Text too long")
+	}
+	return []byte(text)
+}
+
+func int64ToBytes(value int64) (arr []byte) {
+	binary.LittleEndian.PutUint64(arr[0:8], uint64(value))
 	return
 }
 
