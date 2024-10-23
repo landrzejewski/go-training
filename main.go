9aeddf1b6ba45b74e36fcaf1e09076a2a45ac469
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-10-23 14:28:23 +0200

    Basics

--- main.go
+++ main.go
@@ -1,16 +1,99 @@
 package main
 
-import "training.pl/examples/concurrency"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"sync"
+)
+
+const serverAddress = "localhost:9000"
 
 func main() {
-	/*
-		budget := exercises.Budget()
-		var newEntry = exercises.NewBudgetEntry(100.0, exercises.Withdraw, "Cinema")
-		budget.Add(newEntry)
-		budget.FromArgs()
-		budget.PrintSummary()
-		budget.Save()
-	*/
+	client()
+}
 
-	concurrency.UsersDatabase()
+func client() {
+	connection, err := net.Dial("tcp", serverAddress)
+	if err != nil {
+		fmt.Println("Error connecting on:", serverAddress)
+	}
+	defer connection.Close()
+
+	go func() {
+		for {
+			message, err := bufio.NewReader(connection).ReadString('\n')
+			if err != nil {
+				fmt.Println("Disconnected form:", serverAddress)
+				return
+			}
+			fmt.Print("Message: ", message)
+		}
+	}()
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		_, err := fmt.Fprintf(connection, "%s\n", input)
+		if err != nil {
+			fmt.Println("Error sending message:", serverAddress)
+			return
+		}
+	}
+}
+
+func server() {
+	listener, err := net.Listen("tcp", serverAddress)
+	if err != nil {
+		fmt.Println("Error listening on:", serverAddress)
+	}
+	defer listener.Close()
+	fmt.Println("Listening on:", serverAddress)
+
+	broadcast := make(chan string)
+	connections := make([]net.Conn, 0)
+	mutex := sync.Mutex{}
+
+	go func() {
+		for {
+			message := <-broadcast
+			mutex.Lock()
+			for _, connection := range connections {
+				fmt.Println("New message")
+				_, err := fmt.Fprintf(connection, "%s", message)
+				if err != nil {
+					fmt.Println("Error sending:", err)
+				}
+			}
+			mutex.Unlock()
+		}
+	}()
+
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err.Error())
+			os.Exit(1)
+		}
+		fmt.Println("Client connected:", connection.RemoteAddr())
+		mutex.Lock()
+		connections = append(connections, connection)
+		mutex.Unlock()
+		go handleConnection(connection, broadcast)
+	}
+}
+
+func handleConnection(connection net.Conn, broadcast chan<- string) {
+	defer connection.Close()
+	buffer := make([]byte, 1024)
+	for {
+		size, err := connection.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading from connection:", err.Error())
+			os.Exit(1)
+		}
+		message := string(buffer[:size])
+		broadcast <- message
+	}
 }
