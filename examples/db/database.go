a94b682ba6935b839747c5a1d1ea88e33afce006
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2025-05-07 12:51:52 +0200

    concurrency

--- examples/db/database.go
+++ examples/db/database.go
@@ -1,10 +1,12 @@
 package db
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
+	"slices"
 	"sync"
 )
 
@@ -55,6 +57,10 @@ func getLastId(records []Record) int64 {
 	return lastId
 }
 
+func (d *Database) findRecordIndex(id int64) int {
+	return slices.IndexFunc(d.records, func(record Record) bool { return record.Id == id })
+}
+
 func Db(filepath string) (*Database, error) {
 	// Opening data file
 	file, err := os.OpenFile(filepath, os.O_CREATE|os.O_RDWR, 0666)
@@ -102,6 +108,64 @@ func (d *Database) Insert(input interface{}) (*Record, error) {
 	return &record, nil
 }
 
+func (d *Database) FindById(id int64, output interface{}) error {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	index := d.findRecordIndex(id)
+	if index == -1 {
+		return errors.New("Record not found")
+	}
+	record := d.records[index]
+	bytes := make([]byte, record.Length)
+	_, err := d.file.ReadAt(bytes, record.Offset)
+	if err != nil {
+		return err
+	}
+	err = FromBytes(bytes, output)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *Database) DeleteById(id int64) error {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	index := d.findRecordIndex(id)
+	if index == -1 {
+		return errors.New("Record not found")
+	}
+	d.records = append(d.records[:index], d.records[index + 1:]...)
+	d.saveState()
+	return nil
+}
+
+func (d *Database) UpdateById(id int64, input interface{}) error {
+	d.lock.Lock()
+	defer d.lock.Unlock()
+	index := d.findRecordIndex(id)
+	if index == -1 {
+		return errors.New("Record not found")
+	}
+	bytes, err := ToBytes(input)
+	if err != nil {
+		return err
+	}
+	offset, err := d.file.Seek(0, io.SeekEnd)
+	if err != nil {
+		return err
+	}
+	length, err := d.file.WriteAt(bytes, offset)
+	if err != nil {
+		return err
+	}
+	record := &d.records[index]
+	record.Offset = offset
+	record.Length = int64(length)
+	d.saveState()
+	return nil
+}
+
 type User struct {
 	FirstName string
 	LastName  string
@@ -112,7 +176,19 @@ type User struct {
 func DatabaseExample() {
 	db, _ := Db("users.db")
 	defer db.Close()
-	user := User{"Jan", "Kowalski", 25, true}
-	record, _ := db.Insert(&user)
-	fmt.Println(record)
+	// user := User{"Jan", "Kowalski", 25, true}
+	// record, _ := db.Insert(&user)
+	// fmt.Println(record)
+
+	// db.DeleteById(1)
+
+	// db.UpdateById(1, &User{"Jan", "Nowak", 30, false})
+
+	user := User{}
+	err := db.FindById(1, &user)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(user)
 }
